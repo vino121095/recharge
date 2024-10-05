749d693507820bb891e649f15c55b486a1456f2c
VijayDeecodes <166366383+VijayDeecodes@users.noreply.github.com>
2024-10-05 14:05:08 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,9 @@
+const Seequlize = require('sequelize');
+
+const db = new Seequlize('glitztec_recharge', 'glitztec_recharge', 'Boonnet@#12312', {
+    host: "localhost",
+    dialect: "mysql"
+});
+
+
+module.exports = db;
