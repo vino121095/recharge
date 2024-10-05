980aa6fb0051c6f4b86fcf6431dfb0431b543dbe
VijayDeecodes <166366383+VijayDeecodes@users.noreply.github.com>
2024-10-05 14:03:29 +0530

    Delete config/db.js

--- config/db.js
+++
@@ -1,8 +0,0 @@
-const Seequlize = require('sequelize');
-
-const db = new Seequlize('glitztec_recharge', 'glitztec_recharge', 'Boonnet@#12312', {
-    host: "localhost",
-    dialect: "mysql"
-});
-
-module.exports = db;
