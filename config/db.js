2c58a5cb9cb15115bfba20659a3d4de2981a8e10
vino121095 <168708428+vino121095@users.noreply.github.com>
2024-10-07 14:31:53 +0530

    database changes

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const Seequlize = require('sequelize');
 
 const db = new Seequlize('glitztec_recharge', 'glitztec_recharge', 'Boonnet@#12312', {
     host: "localhost",
-    dialect: "mysql"
+    dialect: "mysql",
+    timezone: '+00:00'
 });
-
 
 module.exports = db;
